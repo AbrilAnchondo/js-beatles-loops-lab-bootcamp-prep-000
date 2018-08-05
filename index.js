13f0db8226463c8f43f08563d784d54b8d5b231f
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2018-08-05 04:53:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // add solution here
+function theBeatlesPlay (musicians, instruments) {
+  var newArray = [];
+  for (var i = 0; i < musicians.length; i++){
+   newArray.push(musicians[i] + " plays " + instruments[i]);
+    
+  }
+    return newArray;
+  }
+  
+  function johnLennonFacts (arrayOfFacts) {
+    var i = 0;
+    while (i < arrayOfFacts.length) {
+    arrayOfFacts[i] = arrayOfFacts[i] + "!!!";
+    i++;
+  }
+    return arrayOfFacts;
+  }
+  
+  
